3d1a974ee5e9f6bd98173ac212564486624c2486
rxl3 <12152266+rxl3@users.noreply.github.com>
2025-04-16 14:21:26 +0800

    add scrim config

--- bot/autobook.py
+++ bot/autobook.py
@@ -52,6 +52,7 @@ async def book_serveme(ctx):
                             "password": server_password,
                             "rcon": rcon_password,
                             "first_map": "cp_process_f12",
+                            "server_config": "ozfortress_6v6_scrim"
                         }
                     })
 
