3315a25c8886c00b928cb17781098e18f66d36c3
rxl3 <12152266+rxl3@users.noreply.github.com>
2026-07-28 17:27:41 +0800

    Update stats.py

--- bot/stats/stats.py
+++ bot/stats/stats.py
@@ -307,7 +307,7 @@ async def update_leaderboard(ctx):
 			await lb_channel.send(embed=embed)
 
 async def get_last_map(ctx):
-	match = await db.select_one(('tfmap'), 'qc_matches', where=dict(channel_id=ctx.qc.id))
+	match = await db.select_one(('tfmap',), 'qc_matches', where=dict(channel_id=ctx.qc.id))
 	if not match:
 		return None
 	return match['tfmap']
